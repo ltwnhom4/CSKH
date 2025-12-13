dfc0fcf4589082f851b4a67921ce108d3384c862
nhinhi0601 <86784579+nhinhi0601@users.noreply.github.com>
2025-12-14 00:58:11 +0700

    Clean up URL patterns in urls.py
    Removed redundant paths for danh_sach_thong_bao.

--- TB/urls.py
+++ TB/urls.py
@@ -3,8 +3,6 @@ from . import views
 app_name = 'TB'
 urlpatterns = [
     path('<int:id>/', views.chi_tiet_thong_bao, name='chi_tiet_thong_bao'),
-    path('', views.danh_sach_thong_bao, name='danh_sach_thong_bao'),
-    path('thongbao/', views.danh_sach_thong_bao, name='danh_sach_thong_bao'),
     path('tat-ca/', views.trang_thong_bao, name='trang_thong_bao'),
     path('xem/<int:tb_id>/', views.xem_thong_bao, name='xem_thong_bao'),
     path('tao-khuyen-mai/', views.tao_khuyen_mai, name='tao_khuyen_mai'),
